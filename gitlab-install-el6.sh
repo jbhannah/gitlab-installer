a4bd9c4fe8c461f1f32a28ef87e7a618d9e64a35
Jesse B. Hannah <jesse@jbhannah.net>
2013-06-07 17:32:16 -0600

    Keep yum from automatically installing all recommended packages

--- gitlab-install-el6.sh
+++ gitlab-install-el6.sh
@@ -42,7 +42,7 @@ uname -r | grep "el6" || die 1 "Not RHEL or CentOS 6 (el6)"
 yum -y install http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 
 ## Install build dependencies
-yum -y install patch gcc-c++ readline-devel libffi-devel make autoconf automake libtool bison libxml2-devel libxslt-devel libyaml-devel gettext gettext-devel expat-devel curl-devel zlib-devel openssl-devel cpan
+echo "y" | yum install patch gcc-c++ readline-devel libffi-devel make autoconf automake libtool bison libxml2-devel libxslt-devel libyaml-devel gettext gettext-devel expat-devel curl-devel zlib-devel openssl-devel cpan
 
 ## Install git from source
 echo "yes" | cpan -i ExtUtils::MakeMaker
